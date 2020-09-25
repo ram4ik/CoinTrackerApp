5587a4f3ac2125ced3240e3e379257c7379b00ec
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-25 09:56:47 +0300

    Initial Commit

---
+++ CoinTrackerApp/CoinTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackerAppApp.swift
+//  CoinTrackerApp
+//
+//  Created by ramil on 25.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinTrackerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoinTrackerApp
+//
+//  Created by ramil on 25.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
